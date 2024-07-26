e93d0bd3ee7d37cdb5aaa4d46a80cf98e24751db
emilyzhang625 <139720917+emilyzhang625@users.noreply.github.com>
2024-07-25 22:59:24 -0700

    added remove id to roomReducer but not used, sending messages work but only appear one at a time no chat history and not using dispatch rn

--- src/EnterRoom.jsx
+++ src/EnterRoom.jsx
@@ -1,25 +1,65 @@
-import { useDispatch } from "react-redux";
-import { useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useRef, useState } from "react";
 import { io } from "socket.io-client";
 
 function EnterRoom() {
   const dispatch = useDispatch();
   const roomID = useRef(null);
+  const message = useRef(null);
+  const socket = io("http://localhost:3001");
+  //   const currRoom = useSelector((state) => state.room.id);
+  //   console.log(currRoom);
 
   const handleEnter = () => {
     if (roomID.current.value.trim().length === 0)
       window.alert("Please input a room ID.");
     else {
-      dispatch({ type: "SET_ROOM_ID", payload: roomID.current.value });
-      const socket = io("http://localhost:3001");
-      socket.emit("createOrJoin", roomID.current.value);
-      roomID.current.value = "";
+      socket.emit("createOrJoin", roomID.current.value, (message) => {
+        // dispatch({ type: "SET_ROOM_ID", payload: roomID.current.value });
+
+        window.alert(message);
+      });
     }
   };
+
+  const handleLeave = () => {
+    socket.emit("leaveRoom", roomID.current.value, (message) => {
+      //   dispatch({ type: "REMOVE_ROOM_ID" });
+
+      window.alert(message);
+    });
+  };
+
+  const sendMessage = () => {
+    socket.emit(
+      "sendMessage",
+      roomID.current.value,
+      message.current.value,
+      (cbMessage) => {
+        window.alert(cbMessage);
+        message.current.value = "";
+      }
+    );
+  };
+
+  const [messages, setMessages] = useState([]);
+  socket.on("getMessage", (message) => {
+    setMessages([...messages, message]);
+    console.log(messages);
+  });
+
   return (
     <div>
       <input type="text" placeholder="Room ID" ref={roomID} required></input>
-      <button onClick={handleEnter}>Join/Create Room</button>
+      <button onClick={handleEnter}>Create/Join room</button>
+      <button onClick={handleLeave}>Leave room</button>
+      <input type="text" placeholder="Message" ref={message}></input>
+      <button onClick={sendMessage}>Send</button>
+      <div>
+        {messages.map((message) => (
+          <div key={message}>{message}</div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/store/reducers/roomReducer.js
+++ src/store/reducers/roomReducer.js
@@ -6,6 +6,8 @@ const roomReducer = (state = initialState, action) => {
   switch (action.type) {
     case "SET_ROOM_ID":
       return { id: action.payload };
+    case "REMOVE_ROOM_ID":
+      return { id: "" };
     default:
       return state;
   }
